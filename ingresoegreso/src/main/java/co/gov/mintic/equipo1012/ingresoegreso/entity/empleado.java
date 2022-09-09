40214e44e30f919ba1c676d7ef405080ebbd3d6f
Andres Garzon <verticalrg@yahoo.com>
2022-09-08 23:54:30 -0500

    mensaje

--- ingresoegreso/src/main/java/co/gov/mintic/equipo1012/ingresoegreso/entity/empleado.java
+++ ingresoegreso/src/main/java/co/gov/mintic/equipo1012/ingresoegreso/entity/empleado.java
@@ -1,15 +1,18 @@
 package co.gov.mintic.equipo1012.ingresoegreso.entity;
 
+import java.util.Date;
+
 public class empleado{
     private int id;
     private String nombre;
     private String email;
     private Rol rol;
+    private Date fechaCreacion;
+    private Date fechaActualizacion;
 
     public Rol getRol() {
         return rol;
     }
-
     public void setRol(Rol rol) {
         this.rol = rol;
     }
@@ -17,7 +20,6 @@ public class empleado{
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -25,7 +27,6 @@ public class empleado{
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -33,16 +34,31 @@ public class empleado{
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public empleado(int id, String nombre, String email, Rol rol) {
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+    public void setFechaActualizacion(Date fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public empleado(int id, String nombre, String email, Rol rol, Date fechaCreacion, Date fechaActualizacion) {
         this.id = id;
         this.nombre = nombre;
         this.email = email;
         this.rol = rol;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
 
     }
 
@@ -52,6 +68,8 @@ public class empleado{
                 "id=" + id +
                 ", nombre='" + nombre + '\'' +
                 ", email='" + email + '\'' +
+                ", fechaCreacion=" + fechaCreacion +
+                ", fechaActualizacion=" + fechaActualizacion +
                 '}';
     }
 }
