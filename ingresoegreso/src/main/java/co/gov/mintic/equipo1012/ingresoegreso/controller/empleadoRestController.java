3ba1b06ce1e871a4c8fd5ef0cbb15fb4412d48dc
cdgon76 <cdgon76@hotmail.com>
2022-09-09 23:26:56 -0500

    Actualizacion sprint 3 funcionando

--- ingresoegreso/src/main/java/co/gov/mintic/equipo1012/ingresoegreso/controller/empleadoRestController.java
+++ ingresoegreso/src/main/java/co/gov/mintic/equipo1012/ingresoegreso/controller/empleadoRestController.java
@@ -1,76 +1,48 @@
 package co.gov.mintic.equipo1012.ingresoegreso.controller;
 
-import co.gov.mintic.equipo1012.ingresoegreso.entity.MovimientoDinero;
 import co.gov.mintic.equipo1012.ingresoegreso.entity.empleado;
+import co.gov.mintic.equipo1012.ingresoegreso.service.IempleadoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
 
 public class empleadoRestController {
+
+    @Autowired
+    private IempleadoService empleadoService;
     @GetMapping("/empleado/{id}")
-    public empleado findById(@PathVariable int id) {
+    public empleado findById(@PathVariable long id) {
 
-        Date fechaCreacion = new java.sql.Date(2022-1900,8-1,5);
-        Date fechaActualizacion = new java.sql.Date(2022-1900,8-1,15);
+        return empleadoService.findById(id);
 
-        empleado emp = new empleado(1, "Maria", "midominio@midominio.com", fechaCreacion, fechaActualizacion);
-
-
-        return emp;
     }
 
     @GetMapping("/empleado")
     public List<empleado> findByIdAll() {
 
-        List <empleado> mostrarem = new ArrayList<>();
-
-        Date fechaCreacion = new java.sql.Date(2022-1900,8-1,5);
-        Date fechaActualizacion = new java.sql.Date(2022-1900,8-1,15);
-
-
-        return mostrarem;
+      return empleadoService.findByIdAll();
     }
 
     @PostMapping("/empleado")
     public empleado IngresarEmpleado(@RequestBody empleado emp1){
 
-        Date fechaCreacion = new java.sql.Date(2022-1900,8-1,5);
-        Date fechaActualizacion = new java.sql.Date(2022-1900,8-1,15);
-
-
-        empleado emp = new empleado(4, "Martha", "midominio@midominio.com", fechaCreacion, fechaActualizacion);
-
-        emp1.setId(2);
-        emp1.setNombre(emp1.getNombre());
-        emp1.setEmail(emp1.getEmail());
-        emp1.setFechaCreacion(fechaCreacion);
-        emp1.setFechaActualizacion(fechaActualizacion);
-
-        return emp1;
+        return empleadoService.IngresarEmpleado(emp1);
     }
 
     @PatchMapping("/empleado/{id}/{nombre}")
-    public empleado Actualizar(@PathVariable int id, @PathVariable String nombre){
+    public empleado Actualizar(@PathVariable int id, @RequestBody empleado emp1){
 
-        Date fechaCreacion  = new java.sql.Date(2022,9,2);
-        Date fechaActualizacion = new java.sql.Date(2022,9,3);
-
-        empleado aemp= findById(id);
-        aemp.setNombre(nombre);
-
-        return aemp;
+        return empleadoService.Actualizar(id,emp1);
     }
 
     @DeleteMapping ("/empleado/{id}")
     public void EliminarEmpleado(@PathVariable int id){
 
-        empleado em= findById(id);
-    }
+        empleadoService.EliminarEmpleado(id);
+        }
 }
 
---
+++ ingresoegreso/src/main/java/co/gov/mintic/equipo1012/ingresoegreso/service/empleadoService.java
@@ -0,0 +1,57 @@
+package co.gov.mintic.equipo1012.ingresoegreso.service;
+
+
+import co.gov.mintic.equipo1012.ingresoegreso.entity.empleado;
+import co.gov.mintic.equipo1012.ingresoegreso.repository.IempleadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class empleadoService implements IempleadoService {
+
+    @Autowired
+    private IempleadoRepository empleadoRepository;
+
+    @Override
+
+    public empleado findById(long id) {
+
+        Optional<empleado> emp = empleadoRepository.findById(id);
+
+        return emp.get();
+    }
+
+
+    public List<empleado> findByIdAll() {
+
+        List <empleado> mostrarem = (List<empleado>) empleadoRepository.findAll();
+
+        return mostrarem;
+    }
+
+
+    public empleado IngresarEmpleado(empleado emp1){
+
+       empleado emp = empleadoRepository.save(emp1);
+       return emp1;
+    }
+
+
+    public empleado Actualizar(long id, empleado emp1){
+
+
+        empleado emp = empleadoRepository.save(emp1);
+
+        return emp1;
+    }
+
+
+    public void EliminarEmpleado(long id){
+
+        empleadoRepository.deleteById((long) id);
+    }
+
+}
